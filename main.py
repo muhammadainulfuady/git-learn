bc3f6218f2debdc8ef4e2d27edd5000ad6fe729d
Muhammad Ainul Fuady <ainulfuadi1234@gmail.com>
2026-01-20 09:36:50 +0700

    update file main.py

--- main.py
+++ main.py
@@ -3,3 +3,14 @@ mahasiswa = {
     "jurusan":"Teknik Mesin",
     "hobi":"Ngoding"
 }
+
+
+
+def searchKosonan(data):
+    temp = []
+    kosonan = "aiueoAIUEO"
+    for dt in data:
+        if dt in kosonan:
+            temp.append(dt)
+    return temp
+print(searchKosonan("ainulfuady"))
