efab0b1b01f17216cc5b2a15db9bcfd5ea104ca3
Muhammad Ainul Fuady <ainulfuadi1234@gmail.com>
2026-07-09 22:53:56 +0700

    feat: menambah fitur baru

--- functionEven.js
+++ functionEven.js
@@ -1,3 +1,7 @@
 const dataArray = [12, 3, 1, 65, 7, 90, 2];
 const result = dataArray.filter((n) => n % 2 == 0);
 console.log(result);
+
+const username = "irwan";
+
+console.log(`HAlO ${username}`);
